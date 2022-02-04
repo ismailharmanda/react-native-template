804d6311b544b6198e456cf38ddbe729e13a58c4
ismail <iharmanda@hotmail.com>
2022-02-04 10:16:07 +0300

    template done

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
-
+import ComponentsScreen from "./src/screens/ComponentScreen";
+import ListScreen from "./src/screens/ListScreen";
+import ImageScreen from "./src/screens/ImageScreen";
+import CounterScreen from "./src/screens/CounterScreen";
+import ColorScreen from "./src/screens/ColorScreen";
+import TextScreen from "./src/screens/TextScreen";
+import BoxScreen from "./src/screens/BoxScreen";
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    Components: ComponentsScreen,
+    List: ListScreen,
+    Image: ImageScreen,
+    Counter: CounterScreen,
+    Color: ColorScreen,
+    Text: TextScreen,
+    Box: BoxScreen,
   },
   {
     initialRouteName: "Home",
     defaultNavigationOptions: {
-      title: "App",
+      title: "Fazla Gıda",
     },
   }
 );
 
 export default createAppContainer(navigator);
+2;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,39 @@
-import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+const HomeScreen = (props) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hi there!</Text>
+      <Button
+        onPress={() => props.navigation.navigate("Components")}
+        title="Go to components Demo"
+      />
+      <TouchableOpacity onPressIn={() => props.navigation.navigate("List")}>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
+      <Button
+        title="Go to Image Screen"
+        onPress={() => props.navigation.navigate("Image")}
+      />
+      <Button
+        title="Go to Counter Screen"
+        onPress={() => props.navigation.navigate("Counter")}
+      />
+      <Button
+        title="Go to Color Screen"
+        onPress={() => props.navigation.navigate("Color")}
+      />
+      <Button
+        title="Go to Text Screen"
+        onPress={() => props.navigation.navigate("Text")}
+      />
+      <Button
+        title="Go to Box Screen"
+        onPress={() => props.navigation.navigate("Box")}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
